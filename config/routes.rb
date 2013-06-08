535c4c9fe65d715df31dfa066b2eafea96d19843
Wysaat <Wysaat@gmail.com>
2013-06-08 12:43:17 -0400

    hi

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,12 @@ Microposts::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root to: 'static_pages#home'
+
+  match '/contact', to: 'static_pages#contact'
+  match '/news',    to: 'static_pages#news'
+  match '/about',   to: 'static_pages#about'
+  match '/help',    to: 'static_pages#help'
 
   # See how all your routes lay out with "rake routes"
 
