fee708a6b73bb31d56c5d6119d8c76bef4b6ed42
Wysaat <Wysaat@gmail.com>
2013-06-28 07:35:13 -0700

    branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,10 +20,25 @@ class User < ActiveRecord::Base
 	has_many :reverse_relations, foreign_key: "followee", class_name: "Relation"
 	has_many :followers, through: :reverse_relations, source: :followers
 
-	def follow!(other_user)
-		relations.create!(followed: other_user.name)
-	end
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+    # an alternative way
+	# has_many :followed_users, through: :relationships, source: :followed
+	has_many :followeds, through: :relationships
 
+	#def follow!(other_user)
+	#	relations.create!(followed: other_user.name)
+	#end
+
+    # all 'self's can be omitted
+    def following?(other_user)
+    	self.relationships.find_by_followed_id(other_user.id)
+    end
+
+    def follow!(other_user)
+    	self.relationships.create!(followed_id: other_user.id)
+
+    def unfollow!(other_user)
+    	self.relationships.find_by_followed_id(other_user.id).destroy
 
   private
 
