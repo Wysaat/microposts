f441a67f1dea29f2c4c2ca491a42169351c3bb6c
Wysaat <Wysaat@gmail.com>
2013-07-05 22:24:17 -0700

    dl

--- db/migrate/20130705171525_errorta_checking.rb
+++
@@ -1,9 +0,0 @@
-class ErrortaChecking < ActiveRecord::Migration
-  def up
-    remove_column :microposts, :dowvotes, :integer
-    add_column :microposts, :downvotes, :integer
-  end
-
-  def down
-  end
-end
